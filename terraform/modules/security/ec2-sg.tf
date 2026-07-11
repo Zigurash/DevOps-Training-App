7113bd2825062c7a24cf2f63238cce4a4a97e46a
Viacheslav Andreevich <haus42874287@gmail.com>
2026-07-11 18:28:12 +0700

    sg makes

---
+++ terraform/modules/security/ec2-sg.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "ec2" {
+
+  name        = "${local.name_prefix}-ec2-sg"
+
+  description = "Security group for EC2 instances"
+
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-ec2-sg"
+    }
+  )
+}
